9341a665341214e3ca1a07bc713b9d82a5c48066
Ahmed Mohamed <150616594+I-Ahmed-Mohamed@users.noreply.github.com>
2026-05-08 22:13:49 +0300

    commit

---
+++ data.js
@@ -0,0 +1,614 @@
+const companiesData = [
+  {
+    "name": "امازون",
+    "branches": [
+      {
+        "name": "امازون الشيخ زايد",
+        "link": "https://maps.app.goo.gl/R1g7nEyFAxx3roDw8"
+      },
+      {
+        "name": "امازون جولدن بلازا",
+        "link": "https://maps.app.goo.gl/hqiz5bTjxvTxB2Mr8"
+      },
+      {
+        "name": "امازون المطار",
+        "link": "https://maps.app.goo.gl/2PNy7CBqZxCTb9h97"
+      },
+      {
+        "name": "امازون ارض الجولف",
+        "link": "https://maps.app.goo.gl/E8eDo7GZjhWPbzNJ9"
+      },
+      {
+        "name": "امازون زهراء المعادي",
+        "link": "https://maps.app.goo.gl/U9D1jy2Lrpxoesy88?g_st=ic"
+      },
+      {
+        "name": "امازون الدقي",
+        "link": "https://maps.app.goo.gl/PVqso6XZXeiLsC8T6"
+      },
+      {
+        "name": "امازون طة حسين",
+        "link": "https://maps.app.goo.gl/SNuXyvXfcjvmfLM19"
+      }
+    ]
+  },
+  {
+    "name": "سوبيكو",
+    "branches": [
+      {
+        "name": "سوبيكو الأزهر",
+        "link": "https://maps.app.goo.gl/kFTNSjZ8s5gY7v7C9"
+      },
+      {
+        "name": "سوبيكو شبرا سنجر",
+        "link": "https://maps.app.goo.gl/oGqzwqWAtWcj9vcw9"
+      },
+      {
+        "name": "سوبيكو العباسية",
+        "link": "https://maps.app.goo.gl/ZhKsh5tKwQN4UKLJ6"
+      },
+      {
+        "name": "سوبيكو الوايلي",
+        "link": "https://maps.app.goo.gl/MiW91vzoz37b989XA"
+      },
+      {
+        "name": "سوبيكو النعام",
+        "link": "https://maps.app.goo.gl/4tMGUFh3kt7GqbBy7"
+      },
+      {
+        "name": "سوبيكو الأسمرات",
+        "link": "https://maps.app.goo.gl/G9Hnu1fbT7VxLPgb8"
+      },
+      {
+        "name": "سوبيكو حلوان",
+        "link": "https://maps.app.goo.gl/coa3B3wpyCmjRy3C9"
+      },
+      {
+        "name": "سوبيكو طرة",
+        "link": "https://maps.app.goo.gl/jfqqG2UHMZKwHRK18"
+      },
+      {
+        "name": "سوبيكو شينزو أبي",
+        "link": "https://maps.app.goo.gl/h67vbmypx2ZaSbh68"
+      },
+      {
+        "name": "سوبيكو النزهة",
+        "link": "https://maps.app.goo.gl/c1L1QeRzQXbGHW1U9"
+      },
+      {
+        "name": "سوبيكو التجمع الثالث",
+        "link": "https://maps.app.goo.gl/u9pmXRqzrBjWULJG9"
+      },
+      {
+        "name": "سوبيكو مدينة بدر",
+        "link": "https://maps.app.goo.gl/UDNT4oPZdLgzHVc5A"
+      },
+      {
+        "name": "سوبيكو السلام",
+        "link": "https://maps.app.goo.gl/LECqGaxa313n12cP6"
+      },
+      {
+        "name": "سوبيكو سلام الشهيد أحمد حمدي",
+        "link": "https://maps.app.goo.gl/cHsPZFXx4HguQrWE9"
+      },
+      {
+        "name": "سوبيكو 6 أكتوبر الياسمين",
+        "link": "https://maps.app.goo.gl/wNd3VDCHwSaUtWbQA"
+      },
+      {
+        "name": "سوبيكو سوق المدينة",
+        "link": "https://maps.app.goo.gl/LRRXgbtgVHE2qNH28"
+      },
+      {
+        "name": "سوبيكو العمرانية",
+        "link": "https://maps.app.goo.gl/JPN19Jgc8mUpAeXx8"
+      },
+      {
+        "name": "سوبيكو إمبابة",
+        "link": "https://maps.app.goo.gl/vG8jKmgCescCrpxg8"
+      },
+      {
+        "name": "سوبيكو بشتيل",
+        "link": "https://maps.app.goo.gl/JPN19Jgc8mUpAeXx8"
+      },
+      {
+        "name": "سوبيكو وينجت",
+        "link": "https://maps.app.goo.gl/nCiDdwVLs6Jf8gbf6"
+      },
+      {
+        "name": "سوبيكو سموحة",
+        "link": "https://maps.app.goo.gl/HgJRTZMyhj6ta3na6"
+      },
+      {
+        "name": "سوبيكو سيدي جابر",
+        "link": "https://maps.app.goo.gl/MN2h6NKpjLEhEKWB6"
+      },
+      {
+        "name": "سوبيكو العجمي",
+        "link": "https://maps.app.goo.gl/EGxjKDuVAQd76ukJ9"
+      },
+      {
+        "name": "سوبيكو السيوف",
+        "link": "https://maps.app.goo.gl/GrQfqsMbqZy9yNoc7"
+      },
+      {
+        "name": "سوبيكو محرم بك",
+        "link": "https://maps.app.goo.gl/Q15Z3KTejEY1miGN9"
+      },
+      {
+        "name": "سوبيكو شدس",
+        "link": "https://maps.app.goo.gl/ZRLcmxbtNNHPHtSA7"
+      },
+      {
+        "name": "سوبيكو شبرا الخيمة",
+        "link": "https://maps.app.goo.gl/p5dQf5yW8FQqavjb9"
+      },
+      {
+        "name": "سوبيكو القناطر الخيرية",
+        "link": "https://maps.app.goo.gl/hBPFgHhW1U8uCRk1A"
+      },
+      {
+        "name": "سوبيكو الفيوم",
+        "link": "https://maps.app.goo.gl/EA9mVHimKTF1AU9E7"
+      },
+      {
+        "name": "سوبيكو الزقازيق",
+        "link": "https://maps.app.goo.gl/qnw5YVMgaTxjzD9eA"
+      },
+      {
+        "name": "سوبيكو دمنهور",
+        "link": "https://maps.app.goo.gl/vY5ifKvss5KGDByy7"
+      },
+      {
+        "name": "سوبيكو المنصورة",
+        "link": "https://maps.app.goo.gl/UdLaZuAXfcsdqvEV8"
+      },
+      {
+        "name": "سوبيكو شبين الكوم",
+        "link": "https://maps.app.goo.gl/8nb3biBfC6yeEUdu6"
+      },
+      {
+        "name": "سوبيكو قويسنا",
+        "link": "https://maps.app.goo.gl/mTCxtfx3vCBKFRAD6?g_st=iw"
+      },
+      {
+        "name": "سوبيكو سوتش",
+        "link": "https://maps.app.goo.gl/ZRLcmxbtNNHPHtSA7"
+      },
+      {
+        "name": "سوبيكو المطار",
+        "link": "https://maps.app.goo.gl/Q15Z3KTejEY1miGN9"
+      }
+    ]
+  },
+  {
+    "name": "بانكيكس",
+    "branches": [
+      {
+        "name": "مطعم بانكيكس",
+        "link": "https://maps.google.com/?q=29.967222,30.933388"
+      }
+    ]
+  },
+  {
+    "name": "جودذ مارت",
+    "branches": [
+      {
+        "name": "جودذ مارت",
+        "link": "https://maps.app.goo.gl/9ZW9fjpwHfz6VDZy6?g_st=aw"
+      }
+    ]
+  },
+  {
+    "name": "سوبر سنتر",
+    "branches": [
+      {
+        "name": "سوبر سنتر",
+        "link": "https://maps.app.goo.gl/JySB1qusgonapvMX8?g_st=ic"
+      }
+    ]
+  },
+  {
+    "name": "كارفور",
+    "branches": [
+      {
+        "name": "كارفور المعادي",
+        "link": "   https://share.google/lM2LePFW2Y8uoWKyr   "
+      },
+      {
+        "name": "كارفور مدينة نصر",
+        "link": ""
+      },
+      {
+        "name": "كارفور الماظة",
+        "link": "https://share.google/odfqRIaYQaZgZ7210 "
+      },
+      {
+        "name": "كارفور سيتي لايت",
+        "link": "https://share.google/MwZuzSfdfaMYi4z6L "
+      },
+      {
+        "name": "كارفور سيتي سنتر",
+        "link": "https://share.google/ayYE8quwZnASCiMQc "
+      },
+      {
+        "name": "كارفور داندي مول",
+        "link": "https://share.google/WNdQ6CRMyQOuNQCmL "
+      },
+      {
+        "name": "كارفور العاشر",
+        "link": "https://share.google/LnEybS5nYe1B5tyVR "
+      },
+      {
+        "name": "كارفور السلام",
+        "link": "https://share.google/kpsnmF6PIEF7ZUXxg "
+      },
+      {
+        "name": "كارفور الشروق",
+        "link": "https://share.google/KZ7pihfxvQKhiGlM9 "
+      },
+      {
+        "name": "كارفور العبور",
+        "link": ""
+      },
+      {
+        "name": "كارفور 15 مايو",
+        "link": "https://maps.app.goo.gl/YEjjRgDESY5Z8sTi7?g_st=ic "
+      },
+      {
+        "name": "كارفور سراية القبة",
+        "link": "https://share.google/sYjPOh4kUMrQwcqTl "
+      },
+      {
+        "name": "كارفور مدينتي",
+        "link": "https://maps.app.goo.gl/tCeDERYHAjoXcGhX7?g_st=aw "
+      },
+      {
+        "name": "كارفور كايرو فيستيفال",
+        "link": "https://maps.app.goo.gl/StRzt6HHvrpBNppA7"
+      },
+      {
+        "name": "كارفور اعمار",
+        "link": "https://maps.app.goo.gl/pesmh3ro4j9f99P3A"
+      },
+      {
+        "name": "كارفور مهدي عرفة",
+        "link": "https://share.google/FlXpsMhfDqr3c1v5J"
+      },
+      {
+        "name": "كارفور الأهرام",
+        "link": "https://share.google/PWichvZrQph19H0MJ"
+      },
+      {
+        "name": "كارفور داون تاون",
+        "link": ""
+      },
+      {
+        "name": "ميجا مول كارفور",
+        "link": "https://share.google/dPdgaXdqTVrK0znwR"
+      }
+    ]
+  },
+  {
+    "name": "سبينس",
+    "branches": [
+      {
+        "name": "سبينس بيرلي",
+        "link": "https://share.google/huqjJnfaGS0PtrDIw "
+      },
+      {
+        "name": "سبينس ارينا مول",
+        "link": "https://maps.app.goo.gl/aiJ68jMrFYnpAquw5"
+      },
+      {
+        "name": "سبينس ايناواكس",
+        "link": ""
+      },
+      {
+        "name": "سبينس النزهة",
+        "link": "https://share.google/B4exImiu2s4D1zSzX "
+      },
+      {
+        "name": "سبينس المنصورة",
+        "link": "https://maps.app.goo.gl/dtrhQfWWb4kP9T2b6"
+      },
+      {
+        "name": "سبينس شارع سوريا",
+        "link": "https://share.google/FN6Gx92R9FZAZK15p "
+      },
+      {
+        "name": "سبينس جزيرة العرب",
+        "link": "https://share.google/MdxXbKLvh3esotmO8 "
+      },
+      {
+        "name": "سبينس سيتي سكيب",
+        "link": "https://share.google/18cntKkksDrPK1Zsa "
+      },
+      {
+        "name": "سبينس استرادا مول",
+        "link": "https://maps.app.goo.gl/iu9qButKv5cG6ysg7?g_st=ic "
+      },
+      {
+        "name": "سبينس مكسيم مول",
+        "link": "https://share.google/dk6q8NkCRwrwuAjQf"
+      },
+      {
+        "name": "سبينس مول العرب",
+        "link": "https://share.google/SiX624uvm9aUE1cmG "
+      },
+      {
+        "name": "سبينس الهرم",
+        "link": "https://share.google/RBuUaM7ByGz5KmAET"
+      },
+      {
+        "name": "سبينس مزارا مول",
+        "link": "https://share.google/eGVImVm3V7X8BXVYu"
+      },
+      {
+        "name": "سبينس الشروق",
+        "link": "https://share.google/2owC8SE040ELp5e1M"
+      },
+      {
+        "name": "سبينس العبور",
+        "link": "https://share.google/kpsI2XCK37T0HyH4R"
+      },
+      {
+        "name": "سبينس المعادي",
+        "link": "https://maps.app.goo.gl/AAm4xXPngA7Fw3tk7?g_st=aw"
+      },
+      {
+        "name": "سبينس جري بروجيكت",
+        "link": "https://maps.app.goo.gl/nLumeKaJBYtDdwHq6?g_st=ic"
+      },
+      {
+        "name": "سبينس طنطا",
+        "link": "https://maps.apple/p/VQH1P1fLt_y.bV"
+      }
+    ]
+  },
+  {
+    "name": "أوسكار",
+    "branches": [
+      {
+        "name": "أوسكار المعادي",
+        "link": "https://share.google/AZUvfoJVxLrIhVuVX"
+      },
+      {
+        "name": "أوسكار مدينتي",
+        "link": "https://share.google/ZW748HWaEVm6WiTbv "
+      },
+      {
+        "name": "أوسكار الشيخ زايد",
+        "link": "https://share.google/jvvFBmdHfxW5qSZhs"
+      },
+      {
+        "name": "أوسكار التجمع",
+        "link": "https://share.google/o7rqMmoMVawnf8JoF "
+      },
+      {
+        "name": "أوسكار ايتابا",
+        "link": "https://maps.app.goo.gl/cZ3Ko98UuaZRHnes8?g_st=aw"
+      }
+    ]
+  },
+  {
+    "name": "محمود الفار",
+    "branches": [
+      {
+        "name": "الفار زايد",
+        "link": "https://share.google/H4kCZq3OxyQF6WJ7N "
+      },
+      {
+        "name": "الفار الميرغني",
+        "link": "https://maps.app.goo.gl/45fMwo26TV1EQpUs9"
+      },
+      {
+        "name": "الفار ستريب زون",
+        "link": "https://share.google/5QtUvWG4n90jWHUYk "
+      },
+      {
+        "name": "الفار مدينتي",
+        "link": "https://share.google/SOOiuskhqyMXU41xV "
+      },
+      {
+        "name": "الفار المهندسين",
+        "link": "https://share.google/h3WZXy8CuwytrlKU5 "
+      },
+      {
+        "name": "الفار اربيلا",
+        "link": "https://share.google/wzG0EbMW1Htj2z8BT "
+      },
+      {
+        "name": "الفار ميفيدا",
+        "link": "https://share.google/FEqaHIehnLRHQLx23 "
+      },
+      {
+        "name": "الفار شيراتون",
+        "link": "https://share.google/J6iR7E6lldJ9aCFJa "
+      },
+      {
+        "name": "الفار التسعين الشمالي",
+        "link": "https://share.google/QGgeWogk2GAQpk9fi "
+      },
+      {
+        "name": "الفار التسعين الجنوبي",
+        "link": "https://share.google/U65CAZWMx2Pl6k9OB "
+      }
+    ]
+  },
+  {
+    "name": "بندة سوبر",
+    "branches": [
+      {
+        "name": "بندة الرحاب",
+        "link": "https://share.google/NCwuDf8FUS54ANTqj "
+      },
+      {
+        "name": "بندة أكتوبر",
+        "link": "https://share.google/jrmtdC04elAfdZT7f "
+      },
+      {
+        "name": "بندة المعادي",
+        "link": "https://share.google/s8fVa45ScicKTY8bV "
+      },
+      {
+        "name": "بندة مدينة نصر",
+        "link": "https://share.google/yfR1crC7wgLYHycTq "
+      },
+      {
+        "name": "بندة العبور",
+        "link": "https://share.google/s1pS2lnHndsfcmPQ2 "
+      }
+    ]
+  },
+  {
+    "name": "أولاد رجب",
+    "branches": [
+      {
+        "name": "أولاد رجب حدايق حلوان",
+        "link": "https://maps.app.goo.gl/b76g9CHFHVnn1FLw8?g_st=ic"
+      },
+      {
+        "name": "أولاد رجب الجولف",
+        "link": "https://maps.app.goo.gl/vCpAwhREaeaHZzHX6?g_st=aw"
+      },
+      {
+        "name": "أولاد رجب التجنيد",
+        "link": "https://share.google/jP59oGEzhoS7RhaI9 "
+      },
+      {
+        "name": "أولاد رجب العالمين",
+        "link": "https://maps.app.goo.gl/kGvyTRAFfr9ERTxA9?g_st=ic "
+      },
+      {
+        "name": "أولاد رجب الظاهر العباسية",
+        "link": "https://maps.app.goo.gl/rwTgLSVgFziaRhrX9?g_st=ic "
+      },
+      {
+        "name": "أولاد رجب المعادي",
+        "link": "https://maps.app.goo.gl/sbrHDQ2Qbmbp9Vg38?g_st=aw"
+      }
+    ]
+  },
+  {
+    "name": "اللولو هايبر ماركت",
+    "branches": [
+      {
+        "name": "لولو تاون بلازا",
+        "link": "https://share.google/AAYAYLv204osT86MA"
+      },
+      {
+        "name": "لولو بارك مول",
+        "link": "https://share.google/IHtQPhGPFM6HUECdN"
+      },
+      {
+        "name": "لولو وادي دجلة",
+        "link": "https://share.google/nTTVTftqgJf73lsqf"
+      },
+      {
+        "name": "لولو التجمع الخامس",
+        "link": "https://maps.app.goo.gl/LbTGbUFsDtwSEamo8?g_st=ic"
+      }
+    ]
+  },
+  {
+    "name": "جيان ماركت",
+    "branches": [
+      {
+        "name": "جيان التجمع",
+        "link": "https://share.google/T76rBS4Y06F4tG5hi"
+      },
+      {
+        "name": "جيان الرحاب",
+        "link": "https://share.google/23eY3ZA5N9gp1sdil "
+      }
+    ]
+  },
+  {
+    "name": "زهران ماركت",
+    "branches": [
+      {
+        "name": "زهران العجمي",
+        "link": "https://maps.app.goo.gl/h9vzWPYEWmNkArD56?g_st=aw"
+      },
+      {
+        "name": "زهران سموحة",
+        "link": "https://share.google/lApCPbLAXAlkqdEdU "
+      },
+      {
+        "name": "زهران المعمورة",
+        "link": "https://maps.app.goo.gl/h9vzWPYEWmNkArD56?g_st=aw "
+      },
+      {
+        "name": "زهران المعادي",
+        "link": "https://share.google/otNkUpcoJlQVet9JT "
+      },
+      {
+        "name": "زهران مدينتي",
+        "link": "https://share.google/BNFzs4r2VdrxTU5aZ "
+      },
+      {
+        "name": "زهران زايد",
+        "link": "https://share.google/ZSlZxQw1d5QTxfn7O "
+      },
+      {
+        "name": "زهران التجمع الاول",
+        "link": "https://share.google/RU3K4qiij2MBNMvhU "
+      },
+      {
+        "name": "زهران التجمع الخامس",
+        "link": "https://share.google/8jN5xwVg6BXPdwJF9 "
+      },
+      {
+        "name": "زهران الفراعنة اسكندرية",
+        "link": "https://maps.app.goo.gl/kgRsAdNDmT71YXud8?g_st=aw "
+      },
+      {
+        "name": "زهران زيزينيا اسكندرية",
+        "link": "https://share.google/3FQogR7qncHOfqj5H "
+      },
+      {
+        "name": "زهران سوريا اسكندرية",
+        "link": "https://maps.app.goo.gl/r4eTXfb4dkJvtYyY6?g_st=aw "
+      }
+    ]
+  },
+  {
+    "name": "فندق راديسون",
+    "branches": [
+      {
+        "name": "فندق راديسون",
+        "link": "https://share.google/le5U9HbePBsXhoJtl"
+      }
+    ]
+  },
+  {
+    "name": "وكالة المنصورة",
+    "branches": [
+      {
+        "name": "وكالة المنصورة الهايبر",
+        "link": "https://share.google/PQYYvqHyw7VcBi75e"
+      },
+      {
+        "name": "وكالة المنصورة الترعة",
+        "link": "https://share.google/cSTuHvAtO8s8YUQjb"
+      },
+      {
+        "name": "وكالة المنصورة طالخا",
+        "link": "https://share.google/CPA8kI8t5AInUH0yT"
+      },
+      {
+        "name": "وكالة المنصورة السويس",
+        "link": "https://share.google/pF7KOwnWYcbLFFvZJ"
+      },
+      {
+        "name": "وكالة المنصورة دكرنس",
+        "link": "https://share.google/HfuIu8RGrT9Auc2Pl"
+      },
+      {
+        "name": "وكالة المنصورة بلقاس",
+        "link": "https://share.google/Zd2aBix9ZseUeN5fW"
+      }
+    ]
+  }
+];
